998ca713857d073a635e6428b61f5fa7014a5230
unknown <yzdominic@outlook.com>
2022-07-03 21:32:05 +1000

    finalise minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,20 +4,15 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    // --snip--
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        // --snip--
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
