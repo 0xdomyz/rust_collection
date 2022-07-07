93407e1f860dce37b8fc0d509d1de25d6775ab1b
unknown <yzdominic@outlook.com>
2022-07-07 22:36:53 +1000

    update m inigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,6 +1,4 @@
 use std::env;
-// --snip--
-
 use std::error::Error;
 use std::fs;
 
@@ -11,13 +9,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -46,15 +51,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,12 +4,12 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+
+    // --snip--
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {}", e);
