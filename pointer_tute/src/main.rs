3c37b12be420d427222baf6f1b92ace3f39d5567
unknown <yzdominic@outlook.com>
2022-07-16 22:57:05 +1000

    update pointer tute

--- pointer_tute/src/main.rs
+++ pointer_tute/src/main.rs
@@ -1,3 +1,9 @@
+fn main() {
+    con_usage();
+    use_box_t_as_ref();
+    make_my_box();
+}
+
 enum List {
     Cons(i32, Box<List>),
     Nil,
@@ -5,9 +11,47 @@ enum List {
 
 use crate::List::{Cons, Nil};
 
-fn main() {
+fn con_usage() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 
     let b = Box::new(5);
     println!("b = {}", b);
+
+}
+
+
+fn use_box_t_as_ref() {
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+
+fn make_my_box() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
