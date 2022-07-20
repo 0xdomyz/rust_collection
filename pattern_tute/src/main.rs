ece8a1ff9c33fda1543a3ebd2f9dfab57b050b76
unknown <yzdominic@outlook.com>
2022-07-20 19:20:29 +1000

    add pattern tute

---
+++ pattern_tute/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+    conditional_if_let();
+    while_let();
+    for_loop();
+    function_para();
+}
+
+
+fn conditional_if_let() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {}, as the background", color);
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
+
+
+fn while_let() {
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+}
+
+
+fn for_loop() {
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
+fn function_para() {
+    let point = (3, 5);
+    print_coordinates(&point);
+}
+
+
