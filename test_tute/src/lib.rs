a745957539d1f6d2130b5fdce50747dbaf8617c8
unknown <yzdominic@outlook.com>
2022-07-21 21:04:17 +1000

    add test tute

---
+++ test_tute/src/lib.rs
@@ -0,0 +1,28 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    use super::*;
+    
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+}
+
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
