1c6613efb5c7192fb0399935376e015721978b69
0xdomyz <yzdominic@outlook.com>
2023-01-25 23:34:48 +1100

    udpate structz

---
+++ example/custom_types/enums/enums_type_alias.rs
@@ -0,0 +1,27 @@
+
+enum VeryVerboseEnumOfThingsToDoWithNumbers {
+    Add,
+    Subtract,
+}
+
+impl VeryVerboseEnumOfThingsToDoWithNumbers {
+    fn run(&self, x: i32, y: i32) -> i32 {
+        match self {
+            Self::Add => x + y,
+            Self::Subtract => x - y,
+        }
+    }
+}
+
+// Creates a type alias
+type Operations = VeryVerboseEnumOfThingsToDoWithNumbers;
+
+fn main() {
+    // We can refer to each variant via its alias, not its long and inconvenient
+    // name.
+    let x = Operations::Add;
+    println!("{}", x.run(1, 2));
+    println!("{}", Operations::Subtract.run(1, 2));
+}
+
+
