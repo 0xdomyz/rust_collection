136c54dfa93dace2037a2f2fb658951b59c06356
0xdomyz <yzdominic@outlook.com>
2023-01-31 20:12:03 +1100

    udpate module

--- example/modules/struct_visibility.rs
+++ example/modules/struct_visibility.rs
@@ -28,7 +28,7 @@ fn main() {
 
     // Public structs with private fields cannot be constructed using field names.
     // Error! `ClosedBox` has private fields
-    //let closed_box = my::ClosedBox { contents: "classified information" };
+    // let closed_box = my::ClosedBox { contents: "classified information" };
     // TODO ^ Try uncommenting this line
 
     // However, structs with private fields can be created using
@@ -37,6 +37,6 @@ fn main() {
 
     // and the private fields of a public struct cannot be accessed.
     // Error! The `contents` field is private
-    //println!("The closed box contains: {}", _closed_box.contents);
+    // println!("The closed box contains: {}", _closed_box.contents);
     // TODO ^ Try uncommenting this line
 }
