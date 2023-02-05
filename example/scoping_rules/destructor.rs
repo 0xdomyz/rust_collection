820f2910852122f334db21e58f22bd3cbb7b5be8
0xdomyz <yzdominic@outlook.com>
2023-02-05 14:37:18 +1100

    udpate scope

---
+++ example/scoping_rules/destructor.rs
@@ -0,0 +1,13 @@
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+
+fn main() {
+    let x = ToDrop;
+    println!("Made a ToDrop!");
+    drop(x);
+}
